feb9e8bd0602b9f50e7a7c72b16275c5ee2db94b
Suryaa <suryaacoolness@gmail.com>
2022-12-24 00:52:27 -0500

    Add files via upload

---
+++ OrderBook.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func processCSV(rc io.Reader) (ch chan []string) {
+	ch = make(chan []string, 10)
+	go func() {
+		r := csv.NewReader(rc)
+		if _, err := r.Read(); err != nil { //read header
+			log.Fatal(err)
+		}
+		defer close(ch)
+		for {
+			rec, err := r.Read()
+			if err != nil {
+				if err == io.EOF {
+
+					break
+				}
+				log.Fatal(err)
+
+			}
+			ch <- rec
+		}
+	}()
+	return
+}
+
+func main() {
+
+	csvIn, err := os.Open("EURUSD-2022-11.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(len(processCSV(csvIn)))
+}
