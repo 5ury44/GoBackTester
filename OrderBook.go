c78d0e9da886d402cdc4f21b17fe480639ffac8e
Suryaa <suryaacoolness@gmail.com>
2022-12-25 21:12:53 -0500

    processCSV works and returns map time, [bid, ask]

--- OrderBook.go
+++ OrderBook.go
@@ -6,30 +6,51 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func processCSV(rc io.Reader) (ch chan []string) {
-	ch = make(chan []string, 10)
-	go func() {
-		r := csv.NewReader(rc)
-		if _, err := r.Read(); err != nil { //read header
+func processCSV(rc io.Reader) map[float64][2]float64 {
+
+	processedMap := make(map[float64][2]float64)
+	r := csv.NewReader(rc)
+
+	// handle header
+	rec, err := r.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		rec, err = r.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
 			log.Fatal(err)
 		}
-		defer close(ch)
-		for {
-			rec, err := r.Read()
-			if err != nil {
-				if err == io.EOF {
 
-					break
-				}
-				log.Fatal(err)
+		time := strings.Split(rec[1], ":")
 
-			}
-			ch <- rec
+		floatHour, err := strconv.ParseFloat(time[0][9:], 64)
+		floatMinute, err1 := strconv.ParseFloat(time[1], 64)
+		floatSecond, err2 := strconv.ParseFloat(time[2], 64)
+		if err != nil && err1 != nil && err2 != nil {
+			log.Fatal(err)
 		}
-	}()
-	return
+
+		floatBid, err := strconv.ParseFloat(rec[2], 64)
+		floatAsk, err1 := strconv.ParseFloat(rec[3], 64)
+		if err != nil && err1 != nil {
+			log.Fatal(err)
+		}
+
+		keyTime := 3600*floatHour + 60*floatMinute + floatSecond
+		valBidAsk := [2]float64{floatBid, floatAsk}
+		processedMap[keyTime] = valBidAsk
+
+	}
+	return processedMap
 }
 
 func main() {
