a2095ce1f6c1f49dc5607156804ffed82d89f5f9
Suryaa <suryaacoolness@gmail.com>
2022-12-27 22:14:08 -0500

    todo: fix possible login issue and test

--- LoadCSV.go
+++ LoadCSV.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"golang.org/x/net/html"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -31,8 +33,15 @@ func getCSV(currencies string, current time.Time) {
 
 	client := &http.Client{}
 
+	idcat, err := findIdCat(current.Month().String())
+	if err != nil {
+		fmt.Println("Error parsing webpage:", err)
+		return
+	}
+
 	// Send a GET request to truefx
-	response, err := client.Get("https://www.truefx.com/truefx-historical-downloads/#93-98-april")
+	response, err := client.Get("https://www.truefx.com/truefx-historical-downloads/#93-" +
+		strconv.Itoa(idcat) + "-top")
 	if err != nil {
 		fmt.Println("Error retrieving webpage:", err)
 		return
@@ -93,4 +102,45 @@ func getCSV(currencies string, current time.Time) {
 		fmt.Printf("error writing to file: %v\n", err)
 		return
 	}
+}
+
+func findIdCat(month string) (int, error) {
+
+	resp, err := http.Get("https://www.truefx.com/truefx-historical-downloads/")
+	if err != nil {
+		fmt.Println(err)
+		return -1, nil
+	}
+	defer resp.Body.Close()
+
+	htmlBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return -1, nil
+	}
+
+	tokenizer := html.NewTokenizer(strings.NewReader(string(htmlBody)))
+
+	for {
+		tt := tokenizer.Next()
+		if tt == html.ErrorToken {
+			break
+		}
+		if tt == html.StartTagToken {
+			t := tokenizer.Token()
+			if t.Data == "div" {
+				for _, attr := range t.Attr {
+					if attr.Key == "title" && attr.Val == month {
+						for _, attr := range t.Attr {
+							if attr.Key == "data-idcat" {
+								fmt.Println(attr.Val)
+								return strconv.Atoi(attr.Val)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return -1, nil
 }
